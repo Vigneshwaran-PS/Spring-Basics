8538c47d8fa0530ae91c562d8cc51fc965e3a4ab
Vigneshwaran-PS <psviki29@gmail.com>
2024-01-25 17:47:20 +0530

    Bean-Configuration is created

---
+++ Bean-Configuration/src/main/java/Doctor.java
@@ -0,0 +1,7 @@
+import org.springframework.stereotype.Component;
+
+public class Doctor {
+    public void assist(){
+        System.out.println("Doctor is Asssiting");
+    }
+}
---
+++ Bean-Configuration/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.print.Doc;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+        Doctor doctor = context.getBean(Doctor.class);
+        doctor.assist();
+    }
+}
