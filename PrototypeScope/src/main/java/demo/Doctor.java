8e4735f21b854dfa3e87ebaddf6bffc3a9bcf665
Vigneshwaran-PS <psviki29@gmail.com>
2024-01-27 11:34:55 +0530

    PrototypeScope is updated

--- PrototypeScope/src/main/java/demo/Doctor.java
+++ PrototypeScope/src/main/java/demo/Doctor.java
@@ -1,4 +1,30 @@
 package demo;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(scopeName = "prototype")
 public class Doctor {
+
+    Doctor(){
+        System.out.println("From Doctor Constructor");
+    }
+
+    private String qulaification;
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "qulaification='" + qulaification + '\'' +
+                '}';
+    }
+
+    public String getQulaification() {
+        return qulaification;
+    }
+
+    public void setQulaification(String qulaification) {
+        this.qulaification = qulaification;
+    }
 }
--- PrototypeScope/src/main/java/demo/Main.java
+++ PrototypeScope/src/main/java/demo/Main.java
@@ -1,4 +1,31 @@
 package demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanCongif.class);
+
+        Doctor doctor = context.getBean(Doctor.class);
+        doctor.setQulaification("MBBS");
+        System.out.println(doctor);
+
+        Doctor doctor1 = context.getBean(Doctor.class);
+        System.out.println(doctor1);
+
+
+        // output
+
+        // From Doctor Constructor
+        // Doctor{qulaification='MBBS'}
+        // From Doctor Constructor
+        // Doctor{qulaification='null'}
+
+
+        //Here the contructor is executed twice as defined
+        //for the reference doctor , the qualifiaction is MBBS
+        //for the reference doctor1 , the qualification is null because no value is assigned to the qulaification
+    }
 }
